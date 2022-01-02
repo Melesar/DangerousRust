13dbc3dee05882ef0d4a41237f32c2d77a5b8403
Sergey Pikhota <sergey.pikhota@protonmail.com>
2022-01-02 18:33:12 +0100

    Step 5: Make advance safe

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,69 @@
     non_snake_case
 )]
 
-use std::mem;
 use std::arch::x86_64::*;
 use std::f64::consts::PI;
+
+
+const STARTING_CONDITION : [body; BODIES_COUNT] = [
+    body {    // Sun
+        mass: SOLAR_MASS,
+        position: [0.; 3],
+        velocity: [0.; 3],
+    },
+    body {    // Jupiter
+        position: [
+            4.84143144246472090e+00,
+            -1.16032004402742839e+00,
+            -1.03622044471123109e-01
+        ],
+        velocity: [
+            1.66007664274403694e-03 * DAYS_PER_YEAR,
+            7.69901118419740425e-03 * DAYS_PER_YEAR,
+            -6.90460016972063023e-05 * DAYS_PER_YEAR
+        ],
+        mass: 9.54791938424326609e-04 * SOLAR_MASS,
+    },
+    body {    // Saturn
+        position: [
+            8.34336671824457987e+00,
+            4.12479856412430479e+00,
+            -4.03523417114321381e-01
+        ],
+        velocity: [
+            -2.76742510726862411e-03 * DAYS_PER_YEAR,
+            4.99852801234917238e-03 * DAYS_PER_YEAR,
+            2.30417297573763929e-05 * DAYS_PER_YEAR
+        ],
+        mass: 2.85885980666130812e-04 * SOLAR_MASS
+    },
+    body {    // Uranus
+        position: [
+            1.28943695621391310e+01,
+            -1.51111514016986312e+01,
+            -2.23307578892655734e-01
+        ],
+        velocity: [
+            2.96460137564761618e-03 * DAYS_PER_YEAR,
+            2.37847173959480950e-03 * DAYS_PER_YEAR,
+            -2.96589568540237556e-05 * DAYS_PER_YEAR
+        ],
+        mass: 4.36624404335156298e-05 * SOLAR_MASS
+    },
+    body {    // Neptune
+        position: [
+            1.53796971148509165e+01,
+            -2.59193146099879641e+01,
+            1.79258772950371181e-01
+        ],
+        velocity: [
+            2.68067772490389322e-03 * DAYS_PER_YEAR,
+            1.62824170038242295e-03 * DAYS_PER_YEAR,
+            -9.51592254519715870e-05 * DAYS_PER_YEAR
+        ],
+        mass: 5.15138902046611451e-05 * SOLAR_MASS
+    }
+];
 
 #[repr(C)]
 struct body {
@@ -84,7 +144,8 @@ fn output_Energy(bodies: &[body; BODIES_COUNT]) {
     println!("{:.9}", energy);
 }
 
-unsafe fn advance(bodies: &mut [body; BODIES_COUNT],
+#[cfg(target_feature = "sse2")]
+fn advance(bodies: &mut [body; BODIES_COUNT],
                   position_Deltas: &mut [Interactions; 3],
                   magnitudes: &mut Interactions) {
 
@@ -103,45 +164,67 @@ unsafe fn advance(bodies: &mut [body; BODIES_COUNT],
     }
 
     for i in 0..ROUNDED_INTERACTIONS_COUNT/2 {
-        let mut position_Delta = [_mm_set1_pd(0.); 3];
+        // Safety: this code is only compiled for processors that
+        // support SSE2, so the SIMD operations used here are
+        // safe.
+        let mut position_Delta = unsafe { [_mm_set1_pd(0.); 3] };
         for m in 0..3 {
             position_Delta[m] = position_Deltas[m].as_vectors()[i];
         }
 
-        let position_Delta : [__m128d; 3] = mem::transmute(position_Delta);
-        let distance_Squared : __m128d = _mm_add_pd(
-            _mm_mul_pd(position_Delta[0], position_Delta[0]),
+        // Safety: this code is only compiled for processors that
+        // support SSE2, so the SIMD operations used here are
+        // safe.
+        let distance_Squared : __m128d = unsafe {
             _mm_add_pd(
-                _mm_mul_pd(position_Delta[1], position_Delta[1]),
-                _mm_mul_pd(position_Delta[2], position_Delta[2]),
+                _mm_mul_pd(position_Delta[0], position_Delta[0]),
+                _mm_add_pd(
+                    _mm_mul_pd(position_Delta[1], position_Delta[1]),
+                    _mm_mul_pd(position_Delta[2], position_Delta[2]),
+                    )
             )
-        );
-        let mut distance_Reciprocal : __m128d = 
-            _mm_cvtps_pd(_mm_rsqrt_ps(_mm_cvtpd_ps(distance_Squared)));
+        };
+
+        // Safety: this code is only compiled for processors that
+        // support SSE2, so the SIMD operations used here are
+        // safe.
+        let mut distance_Reciprocal : __m128d = unsafe {
+            _mm_cvtps_pd(_mm_rsqrt_ps(_mm_cvtpd_ps(distance_Squared)))
+        };
+
         for _ in 0..2 {
-            distance_Reciprocal = _mm_sub_pd(
-                _mm_mul_pd(distance_Reciprocal, _mm_set1_pd(1.5)),
-                _mm_mul_pd(
+            // Safety: this code is only compiled for processors that
+            // support SSE2, so the SIMD operations used here are
+            // safe.
+            distance_Reciprocal = unsafe {
+                _mm_sub_pd(
+                    _mm_mul_pd(distance_Reciprocal, _mm_set1_pd(1.5)),
                     _mm_mul_pd(
                         _mm_mul_pd(
-                            _mm_set1_pd(0.5),
-                            distance_Squared
+                            _mm_mul_pd(
+                                _mm_set1_pd(0.5),
+                                distance_Squared
+                            ),
+                            distance_Reciprocal
                         ),
-                        distance_Reciprocal
-                    ),
-                    _mm_mul_pd(distance_Reciprocal, distance_Reciprocal)
+                        _mm_mul_pd(distance_Reciprocal, distance_Reciprocal)
+                      )
                 )
-            );
+            };
         }
 
-        magnitudes.as_vectors()[i] = 
+        // Safety: this code is only compiled for processors that
+        // support SSE2, so the SIMD operations used here are
+        // safe.
+        magnitudes.as_vectors()[i] = unsafe {
             _mm_mul_pd(
                 _mm_div_pd(
                     _mm_set1_pd(0.01),
                     distance_Squared
                 ),
                 distance_Reciprocal
-            );
+            )
+        };
     }
 
     {
@@ -168,66 +251,7 @@ unsafe fn advance(bodies: &mut [body; BODIES_COUNT],
 
 
 fn main() {
-    let mut solar_Bodies : [body; BODIES_COUNT] = [
-        body {    // Sun
-            mass: SOLAR_MASS,
-            position: [0.; 3],
-            velocity: [0.; 3],
-        },
-        body {    // Jupiter
-            position: [
-                4.84143144246472090e+00,
-                -1.16032004402742839e+00,
-                -1.03622044471123109e-01
-            ],
-            velocity: [
-                1.66007664274403694e-03 * DAYS_PER_YEAR,
-                7.69901118419740425e-03 * DAYS_PER_YEAR,
-                -6.90460016972063023e-05 * DAYS_PER_YEAR
-            ],
-            mass: 9.54791938424326609e-04 * SOLAR_MASS,
-        },
-        body {    // Saturn
-            position: [
-                8.34336671824457987e+00,
-                4.12479856412430479e+00,
-                -4.03523417114321381e-01
-            ],
-            velocity: [
-                -2.76742510726862411e-03 * DAYS_PER_YEAR,
-                4.99852801234917238e-03 * DAYS_PER_YEAR,
-                2.30417297573763929e-05 * DAYS_PER_YEAR
-            ],
-            mass: 2.85885980666130812e-04 * SOLAR_MASS
-        },
-        body {    // Uranus
-            position: [
-                1.28943695621391310e+01,
-                -1.51111514016986312e+01,
-                -2.23307578892655734e-01
-            ],
-            velocity: [
-                2.96460137564761618e-03 * DAYS_PER_YEAR,
-                2.37847173959480950e-03 * DAYS_PER_YEAR,
-                -2.96589568540237556e-05 * DAYS_PER_YEAR
-            ],
-            mass: 4.36624404335156298e-05 * SOLAR_MASS
-        },
-        body {    // Neptune
-            position: [
-                1.53796971148509165e+01,
-                -2.59193146099879641e+01,
-                1.79258772950371181e-01
-            ],
-            velocity: [
-                2.68067772490389322e-03 * DAYS_PER_YEAR,
-                1.62824170038242295e-03 * DAYS_PER_YEAR,
-                -9.51592254519715870e-05 * DAYS_PER_YEAR
-            ],
-            mass: 5.15138902046611451e-05 * SOLAR_MASS
-        }
-    ];
-
+    let mut solar_Bodies = STARTING_CONDITION;
     let mut position_Deltas = [Interactions {scalars: [0.; ROUNDED_INTERACTIONS_COUNT]}; 3];
     let mut magnitudes = Interactions {scalars: [0.; ROUNDED_INTERACTIONS_COUNT]};
 
@@ -235,10 +259,8 @@ fn main() {
     output_Energy(&solar_Bodies);
 
     let c = std::env::args().nth(1).unwrap().parse().unwrap();
-    unsafe {
-        for _ in 0..c {
-            advance(&mut solar_Bodies, &mut position_Deltas, &mut magnitudes);
-        }
+    for _ in 0..c {
+        advance(&mut solar_Bodies, &mut position_Deltas, &mut magnitudes);
     }
     
     output_Energy(&solar_Bodies);
